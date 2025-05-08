ab9e70a0da9025aa991917515c4c96436d8b7d15
Mohini <mohinikar1497@gmail.com>
2025-05-08 11:00:42 +0530

    first commit from mohini

--- src/deposit/saving.java
+++ src/deposit/saving.java
@@ -5,6 +5,7 @@ public class saving {
 	public static void main(String[] args) {
 		System.out.println("welcmone to deposit");
 		System.out.println("savings");
+		System.out.println("clonned");
 
 	}
 
